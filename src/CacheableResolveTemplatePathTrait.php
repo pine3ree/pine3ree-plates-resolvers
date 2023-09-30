4380b05bd04f4a6e2e609ea9ec38714075d6f018
pine3ree <pine3ree@gmail.com>
2023-10-01 00:51:56 +0200

    Add trait implementing the cacheable interface methods
    Signed-off-by: pine3ree <pine3ree@gmail.com>

---
+++ src/CacheableResolveTemplatePathTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package pine3ree-plates-resolvers
+ * @author  pine3ree https://github.com/pine3ree
+ * @copyright (c) 2023, pine3ree
+ * @license https://github.com/pine3ree/pine3ree-plates-resolvers/blob/master/LICENSE.md BSD 3-Clause License
+ */
+
+namespace pine3ree\Plates\Template\ResolveTemplatePath;
+
+/**
+ * Implements CacheableResolveTemplatePathInterface methods
+ */
+trait CacheableResolveTemplatePathTrait
+{
+    /**
+     * A map of positively resolved template-paths indexed by template-name
+     */
+    protected array $cache = [];
+
+    public function addToCache(string $templateName, string $templatePath): void
+    {
+        $this->cache[$templateName] = $templatePath;
+    }
+
+    public function getFromCache(string $templateName): ?string
+    {
+        return $this->cache[$templateName] ?? null;
+    }
+
+    public function clearCache(): void
+    {
+        $this->cache = [];
+    }
+}
